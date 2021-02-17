bdec6a4b13dc45ec491fc5d4e222eb9ffc547372
Justin McMurdie <mcmurdiej@gmail.com>
2021-02-17 07:50:49 -0700

    refactoring v17 to work with v16

--- src/react-dom-server.browser.development.js
+++ src/react-dom-server.browser.development.js
@@ -1,6 +1,10 @@
 import ReactDOMServer from "react-dom/cjs/react-dom-server.browser.development.js";
 
 export default ReactDOMServer;
-export * from "react-dom/cjs/react-dom-server.browser.development.js";
-// This file exists simply to apply __esModule: true to the module
+export const {
+  renderToString,
+  renderToStaticMarkup,
+  renderToNodeStream,
+  renderToStaticNodeStream,
+} = ReactDOMServer;
 export const __esModule = true;
